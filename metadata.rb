6be554cf75bf0fdda9d2d962a828a24fb28a6988
root <root@chef-workstation.dexter.com.br>
2018-04-01 09:38:50 -0300

    Configuracao Base de usurios, Pacotes e Hosts

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'confsbase-marfebr'
+maintainer 'Aislan Max'
+maintainer_email 'marfebr@gmail.com'
+license 'All Rights Reserved'
+description 'Configuracao base'
+long_description 'Configuracao base de usuarios, pacotes e hosts'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url "https://github.com/marfebr/confsbase-marfebr"
